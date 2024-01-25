796e722c13a8d88715ccb7cd2a2bf83433b8b37e
Arfan Abid <iffionex111@gmail.com>
2024-01-25 16:57:47 +0500

    ~ Session Auth and Authentication

--- Backend/core/products/urls.py
+++ Backend/core/products/urls.py
@@ -2,8 +2,8 @@ from django .urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductMixinViews.as_view()),
+    path('', views.ProductListCreateAPIView.as_view()),
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/update/', views.ProductMixinViews.as_view()), # Reterieve Mode
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()), # Reterieve Mode
     path('<int:pk>/delete/', views.ProductDestroyAPIView.as_view()),
 ]
