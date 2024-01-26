854e9482d9a56562c0b2d3843d9de77fd8464b3e
Arfan Abid <iffionex111@gmail.com>
2024-01-26 13:28:25 +0500

    ~ Configered Token Authentication

--- py_client/create.py
+++ py_client/create.py
@@ -1,11 +1,12 @@
 import requests
 
 endpoint="http://localhost:8000/api/products/" 
+headers={'Authorization': 'Bearer dc3074ad161dbe4128977f6c92a6e89660e8275a'}
 data={
     "title":"THis field is done",
     "content":"DRF related content"
 }
 
-get_response=requests.post(endpoint,json=data) # HTTP request
+get_response=requests.post(endpoint,json=data,headers=headers) # HTTP request
 print(get_response.json()) #print json response
 
