93011388aea85214a87628b6288e31635d76e81f
Arfan Abid <iffionex111@gmail.com>
2024-02-01 23:48:30 +0500

    ~ Custom Validation with serializers

--- Backend/core/products/serializers.py
+++ Backend/core/products/serializers.py
@@ -2,25 +2,34 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from  .models import Product
+from  . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount=serializers.SerializerMethodField(read_only=True)
     edit_url=serializers.SerializerMethodField(read_only=True)
     url= serializers.HyperlinkedIdentityField(view_name='product-detail',lookup_field='pk')
+    title=serializers.CharField(validators.validate_title_no_hello,validators.unique_product_title)
     email=serializers.EmailField(wite_only=True)
     class Meta:
         model=Product
         fields=('url','edit_url','email','pk','title','content','price','sale_price','my_discount',)
 
-    def create(self,validated_data):
-        # return Product.objects.create(**validated_data)
-        # email=validated_data.pop('email')
-        obj=super().create(validated_data)
-        return obj
+    def validate_title(self, value):
+        request=self.context.get('request') 
+        user=request.user
+        qs=Product.objects.filter(uaer=user,title__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError({"title": "This title is already taken"})
+        return value
+    # def create(self,validated_data):
+    #     # return Product.objects.create(**validated_data)
+    #     # email=validated_data.pop('email')
+    #     obj=super().create(validated_data)
+    #     return obj 
     
-    def update(self,instance,validated_data):
-        # email=validated_data.pop('email')
-        return super().update(instance,validated_data)
+    # def update(self,instance,validated_data):
+    #     # email=validated_data.pop('email')
+    #     return super().update(instance,validated_data)
         
     def get_edit_url(self,obj):
         # return f"/api/v2/products/{obj.pk}/"
